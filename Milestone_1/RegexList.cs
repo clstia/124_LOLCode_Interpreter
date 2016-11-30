422e8e1ffacbd2cd9e30835a905b1b3fee8300fc
clstia <janelliseurolfan@gmail.com>
2016-11-30 11:07:06 +0800

    Added some regex

--- Milestone_1/RegexList.cs
+++ Milestone_1/RegexList.cs
@@ -5,154 +5,160 @@ namespace Milestone_1
 {
 	public class RegexList : Hashtable
 	{
-		public Hashtable regexHT, varHT, reservedHT, syntaxHT;
+		public Hashtable genericHT;
 
 		public RegexList ()
 		{
 			
 		}
 
-		public Hashtable initSyntaxHT ()
+		public Hashtable initArithmetic ()
 		{
-			syntaxHT = new Hashtable ();
+			genericHT = new Hashtable ();
+			return genericHT;
+		}
+
+		public Hashtable initIHasHT ()
+		{
+			genericHT = new Hashtable ();
 
 			// I HAS A
-			syntaxHT.Add (@"^I HAS A [a-zA-Z][a-zA-Z0-9_]*$", "NOOB");
-			syntaxHT.Add (@"^I HAS A [a-zA-Z][a-zA-Z0-9_]* ITZ (WIN|FAIL)", "TROOF");
-			syntaxHT.Add ("^I HAS A [a-zA-Z][a-zA-Z0-9_]* ITZ \".*\"$", "YARN");
-			syntaxHT.Add (@"^I HAS A [a-zA-Z][a-zA-Z0-9_]* ITZ (-)?[0-9]+$", "NUMBR");
-			syntaxHT.Add (@"^I HAS A [a-zA-Z][a-zA-Z0-9_]* ITZ (-)?[0-9]+\.[0-9]+$", "NUMBAR"); 
+			genericHT.Add (@"^I HAS A [a-zA-Z][a-zA-Z0-9_]*$", "NOOB");
+			genericHT.Add (@"^I HAS A [a-zA-Z][a-zA-Z0-9_]* ITZ (WIN|FAIL)", "TROOF");
+			genericHT.Add ("^I HAS A [a-zA-Z][a-zA-Z0-9_]* ITZ \".*\"$", "YARN");
+			genericHT.Add (@"^I HAS A [a-zA-Z][a-zA-Z0-9_]* ITZ (-)?[0-9]+$", "NUMBR");
+			genericHT.Add (@"^I HAS A [a-zA-Z][a-zA-Z0-9_]* ITZ (-)?[0-9]+\.[0-9]+$", "NUMBAR"); 
 			// add init with expression
 
-			return syntaxHT;
+			return genericHT;
 		}
 
 		public Hashtable initReservedHT ()
 		{
-			reservedHT = new Hashtable ();
+			genericHT = new Hashtable ();
 
-			reservedHT.Add (@"^HAI$", "Reserved"); 
-			reservedHT.Add (@"^KTHXBYE$", "Reserved");
-			reservedHT.Add (@"^BTW$", "Reserved");
-			reservedHT.Add (@"^OBTW$", "Reserved");
-			reservedHT.Add (@"^TLDR$", "Reserved");
-			reservedHT.Add (@"^VISIBLE$", "Reserved");
-			reservedHT.Add (@"^GIMMEH$", "Reserved");
-			reservedHT.Add (@"^I$", "Reserved");
-			reservedHT.Add (@"^HAS$", "Reserved");
-			reservedHT.Add (@"^A$", "Reserved");
-			reservedHT.Add (@"^ITZ$", "Reserved");
-			reservedHT.Add (@"^R$", "Reserved");
-			reservedHT.Add (@"^SMOOSH$", "Reserved");
-			reservedHT.Add (@"^O$", "Reserved");
-			reservedHT.Add (@"^RLY$", "Reserved");
-			reservedHT.Add (@"^YA$", "Reserved");
-			reservedHT.Add (@"^NO$", "Reserved");
-			reservedHT.Add (@"^WAI$", "Reserved");
-			reservedHT.Add (@"^WTF$", "Reserved");
-			reservedHT.Add (@"^OMG$", "Reserved");
-			reservedHT.Add (@"^GTFO$", "Reserved");
-			reservedHT.Add (@"^OMGWTF$", "Reserved");
-			reservedHT.Add (@"^OIC$", "Reserved");
-			reservedHT.Add (@"^IT$", "Reserved");
-			reservedHT.Add (@"^AN$", "Reserved");
-			reservedHT.Add (@"^OF$", "Reserved");
-			reservedHT.Add (@"^SUM$", "Reserved");
-			reservedHT.Add (@"^DIFF$", "Reserved");
-			reservedHT.Add (@"^PRODUKT$", "Reserved");
-			reservedHT.Add (@"^QUOSHUNT$", "Reserved");
-			reservedHT.Add (@"^MOD$", "Reserved");
-			reservedHT.Add (@"^BIGGR$", "Reserved");
-			reservedHT.Add (@"^SMALLR$", "Reserved");
-			reservedHT.Add (@"^BOTH$", "Reserved");
-			reservedHT.Add (@"^EITHER$", "Reserved");
-			reservedHT.Add (@"^ANY$", "Reserved");
-			reservedHT.Add (@"^ALL$", "Reserved");
-			reservedHT.Add (@"^NOT$", "Reserved");
-			reservedHT.Add (@"^WON$", "Reserved");
-			reservedHT.Add (@"^SAEM$", "Reserved");
-			reservedHT.Add (@"^DIFFRINT$", "Reserved");
+			genericHT.Add (@"^HAI$", "Reserved"); 
+			genericHT.Add (@"^KTHXBYE$", "Reserved");
+			genericHT.Add (@"^BTW$", "Reserved");
+			genericHT.Add (@"^OBTW$", "Reserved");
+			genericHT.Add (@"^TLDR$", "Reserved");
+			genericHT.Add (@"^VISIBLE$", "Reserved");
+			genericHT.Add (@"^GIMMEH$", "Reserved");
+			genericHT.Add (@"^I$", "Reserved");
+			genericHT.Add (@"^HAS$", "Reserved");
+			genericHT.Add (@"^A$", "Reserved");
+			genericHT.Add (@"^ITZ$", "Reserved");
+			genericHT.Add (@"^R$", "Reserved");
+			genericHT.Add (@"^SMOOSH$", "Reserved");
+			genericHT.Add (@"^O$", "Reserved");
+			genericHT.Add (@"^RLY$", "Reserved");
+			genericHT.Add (@"^YA$", "Reserved");
+			genericHT.Add (@"^NO$", "Reserved");
+			genericHT.Add (@"^WAI$", "Reserved");
+			genericHT.Add (@"^WTF$", "Reserved");
+			genericHT.Add (@"^OMG$", "Reserved");
+			genericHT.Add (@"^GTFO$", "Reserved");
+			genericHT.Add (@"^OMGWTF$", "Reserved");
+			genericHT.Add (@"^OIC$", "Reserved");
+			genericHT.Add (@"^IT$", "Reserved");
+			genericHT.Add (@"^AN$", "Reserved");
+			genericHT.Add (@"^OF$", "Reserved");
+			genericHT.Add (@"^SUM$", "Reserved");
+			genericHT.Add (@"^DIFF$", "Reserved");
+			genericHT.Add (@"^PRODUKT$", "Reserved");
+			genericHT.Add (@"^QUOSHUNT$", "Reserved");
+			genericHT.Add (@"^MOD$", "Reserved");
+			genericHT.Add (@"^BIGGR$", "Reserved");
+			genericHT.Add (@"^SMALLR$", "Reserved");
+			genericHT.Add (@"^BOTH$", "Reserved");
+			genericHT.Add (@"^EITHER$", "Reserved");
+			genericHT.Add (@"^ANY$", "Reserved");
+			genericHT.Add (@"^ALL$", "Reserved");
+			genericHT.Add (@"^NOT$", "Reserved");
+			genericHT.Add (@"^WON$", "Reserved");
+			genericHT.Add (@"^SAEM$", "Reserved");
+			genericHT.Add (@"^DIFFRINT$", "Reserved");
 
-			return reservedHT;
+			return genericHT;
 		}
 
 		public Hashtable initVarHT ()
 		{
-			varHT = new Hashtable ();
+			genericHT = new Hashtable ();
 
 			// variables
-			varHT.Add ("^\".*\"$", "YARN");
-			varHT.Add (@"^(-)?[0-9]+$", "NUMBR");
-			varHT.Add (@"^(-)?[0-9]+\.[0-9]+$", "NUMBAR");
-			varHT.Add (@"^(WIN|FAIL)$", "TROOF");
-			varHT.Add (@"^[a-zA-Z][a-zA-Z0-9_]+$", "NOOB");
+			genericHT.Add ("^\".*\"$", "YARN");
+			genericHT.Add (@"^(-)?[0-9]+$", "NUMBR");
+			genericHT.Add (@"^(-)?[0-9]+\.[0-9]+$", "NUMBAR");
+			genericHT.Add (@"^(WIN|FAIL)$", "TROOF");
+			genericHT.Add (@"^[a-zA-Z][a-zA-Z0-9_]+$", "NOOB");
 
-			return varHT;
+			return genericHT;
 		}
 
 		public Hashtable initRegexHT ()
 		{
-			regexHT = new Hashtable ();
+			genericHT = new Hashtable ();
 
 			// comparative operations
-			regexHT.Add (@"^BIGGR OF$", "Max"); // m1 done. need to detect arguments
-			regexHT.Add (@"^SMALLR OF$", "Min"); // m1 done . need to detect arguments
-			regexHT.Add (@"^BOTH OF$", "Boolean AND"); // m1 done . need to detect arguments
-			regexHT.Add (@"^EITHER OF$", "Boolean OR"); // m1 done . need to detect arguments
-			regexHT.Add (@"^WON OF$", "Boolean XOR"); // m1 done . need to detect arguments
-			regexHT.Add (@"^NOT$", "Boolean NOT"); // m1 done . need to detect arguments
-			regexHT.Add (@"^ANY OF$", "N-Arity Boolean OR"); // m1 done . need to detect arguments
-			regexHT.Add (@"^ALL OF$", "N-Arity Boolean AND"); // m1 done . need to detect arguments
-			regexHT.Add (@"^BOTH SAEM$", "Equality"); // m1 done . need to detect arguments
-			regexHT.Add (@"^DIFFRINT$", "Inequality");  // m1 done . need to detect arguments
+			genericHT.Add (@"^BIGGR OF$", "Max"); // m1 done. need to detect arguments
+			genericHT.Add (@"^SMALLR OF$", "Min"); // m1 done . need to detect arguments
+			genericHT.Add (@"^BOTH OF$", "Boolean AND"); // m1 done . need to detect arguments
+			genericHT.Add (@"^EITHER OF$", "Boolean OR"); // m1 done . need to detect arguments
+			genericHT.Add (@"^WON OF$", "Boolean XOR"); // m1 done . need to detect arguments
+			genericHT.Add (@"^NOT$", "Boolean NOT"); // m1 done . need to detect arguments
+			genericHT.Add (@"^ANY OF$", "N-Arity Boolean OR"); // m1 done . need to detect arguments
+			genericHT.Add (@"^ALL OF$", "N-Arity Boolean AND"); // m1 done . need to detect arguments
+			genericHT.Add (@"^BOTH SAEM$", "Equality"); // m1 done . need to detect arguments
+			genericHT.Add (@"^DIFFRINT$", "Inequality");  // m1 done . need to detect arguments
 
 			// start and end of code
-			regexHT.Add (@"^HAI$", "Start of Code"); // m1 done
-			regexHT.Add (@"^KTHXBYE$", "End of Code"); // m1 done
+			genericHT.Add (@"^HAI$", "Start of Code"); // m1 done
+			genericHT.Add (@"^KTHXBYE$", "End of Code"); // m1 done
 
 			// comments
-			regexHT.Add (@"^BTW .*", "Single Line Comment"); // m1 done
-			regexHT.Add (@"^OBTW$", "Start of Block Comment"); // m1 done
-			regexHT.Add (@"^TLDR$", "End of Block Comment"); // m1 done
+			genericHT.Add (@"^BTW .*", "Single Line Comment"); // m1 done
+			genericHT.Add (@"^OBTW$", "Start of Block Comment"); // m1 done
+			genericHT.Add (@"^TLDR$", "End of Block Comment"); // m1 done
 
 			// standard input and output
-			regexHT.Add (@"^VISIBLE", "Standard Output"); // m1 done
-			regexHT.Add (@"^GIMMEH [a-zA-Z][a-zA-Z0-9_]*", "Standard Input"); // m1 done
+			genericHT.Add (@"^VISIBLE", "Standard Output"); // m1 done
+			genericHT.Add (@"^GIMMEH [a-zA-Z][a-zA-Z0-9_]*", "Standard Input"); // m1 done
 
 			// variable operations
-			regexHT.Add (@"^I HAS A [a-zA-Z][a-zA-Z0-9_]*$", "Variable Declaration"); // m1 done 
-			regexHT.Add (@"^ITZ", "Initialization"); // m1 done
-			regexHT.Add (@"^R$", "Assignment Statement"); // m1 done
-			regexHT.Add (@"^SMOOSH .*", "String Concatenation");  // m1 done
+			genericHT.Add (@"^I HAS A [a-zA-Z][a-zA-Z0-9_]*$", "Variable Declaration"); // m1 done 
+			genericHT.Add (@"^ITZ", "Initialization"); // m1 done
+			genericHT.Add (@"^R$", "Assignment Statement"); // m1 done
+			genericHT.Add (@"^SMOOSH .*", "String Concatenation");  // m1 done
 
 			// control flow structures
-			regexHT.Add (@"^O RLY\?$", "Start of IF Block"); //m1 done
-			regexHT.Add (@"^YA RLY$", "IF Clause"); // m1 done
-			regexHT.Add (@"^NO WAI$", "ELSE Clause"); // m1 done
-			regexHT.Add (@"^WTF\?$", "Start of CASE Block"); // m1 done
-			regexHT.Add (@"^OMG$", "CASE Clause"); // m1 done
-			regexHT.Add (@"^GTFO$", "BREAK Clause"); // m1 done
-			regexHT.Add (@"^OMGWTF$", "DEFAULT Clause"); // m1 done
-			regexHT.Add (@"^OIC$", "End of Control Block"); // m1 done
+			genericHT.Add (@"^O RLY\?$", "Start of IF Block"); //m1 done
+			genericHT.Add (@"^YA RLY$", "IF Clause"); // m1 done
+			genericHT.Add (@"^NO WAI$", "ELSE Clause"); // m1 done
+			genericHT.Add (@"^WTF\?$", "Start of CASE Block"); // m1 done
+			genericHT.Add (@"^OMG$", "CASE Clause"); // m1 done
+			genericHT.Add (@"^GTFO$", "BREAK Clause"); // m1 done
+			genericHT.Add (@"^OMGWTF$", "DEFAULT Clause"); // m1 done
+			genericHT.Add (@"^OIC$", "End of Control Block"); // m1 done
 
 			// argument separator
-			regexHT.Add (@"^AN$", "Argument Separator"); // m1 done
+			genericHT.Add (@"^AN$", "Argument Separator"); // m1 done
 
 			// arithmetic operations
-			regexHT.Add (@"^SUM OF$", "Addition"); // m1 done
-			regexHT.Add (@"^DIFF OF$", "Subtraction"); // m1 done
-			regexHT.Add (@"^PRODUKT OF$", "Multiplication"); // m1 done
-			regexHT.Add (@"^QUOSHUNT OF$", "Division"); // m1 done
-			regexHT.Add (@"^MOD OF$", "Modulo Division"); // m1 done
+			genericHT.Add (@"^SUM OF$", "Addition"); // m1 done
+			genericHT.Add (@"^DIFF OF$", "Subtraction"); // m1 done
+			genericHT.Add (@"^PRODUKT OF$", "Multiplication"); // m1 done
+			genericHT.Add (@"^QUOSHUNT OF$", "Division"); // m1 done
+			genericHT.Add (@"^MOD OF$", "Modulo Division"); // m1 done
 
 			// variables
-			regexHT.Add ("^\".*\"$", "YARN"); // m1 done
-			regexHT.Add (@"^(-)?[0-9]+$", "NUMBR"); // m1 done
-			regexHT.Add (@"^(-)?[0-9]+\.[0-9]+$", "NUMBAR"); // m1 done
-			regexHT.Add (@"^(WIN|FAIL)$", "TROOF"); // m1 done
-			regexHT.Add (@"^[a-zA-Z][a-zA-Z0-9_]*$", "NOOB");
+			genericHT.Add ("^\".*\"$", "YARN"); // m1 done
+			genericHT.Add (@"^(-)?[0-9]+$", "NUMBR"); // m1 done
+			genericHT.Add (@"^(-)?[0-9]+\.[0-9]+$", "NUMBAR"); // m1 done
+			genericHT.Add (@"^(WIN|FAIL)$", "TROOF"); // m1 done
+			genericHT.Add (@"^[a-zA-Z][a-zA-Z0-9_]*$", "NOOB");
 
-			return regexHT;
+			return genericHT;
 		}
 	}
 }
